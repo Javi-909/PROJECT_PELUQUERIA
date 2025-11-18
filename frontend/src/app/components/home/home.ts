718cb452918056e1ea0c10902fdca7bf3b5f7433
Javi-909 <javiquevedo09@gmail.com>
2025-11-18 12:52:16 +0100

    Barra de búsqueda sin funcionar

--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PeluqueriaCardComponent } from '../peluqueria-card/peluqueria-card';
 import { PeluqueriaService } from '../../services/peluqueria';
 import { Peluqueria } from '../../models/peluqueria.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, PeluqueriaCardComponent],
+  imports: [CommonModule, PeluqueriaCardComponent, FormsModule],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
@@ -15,6 +16,12 @@ export class HomeComponent implements OnInit {
 
   private peluqueriaService = inject(PeluqueriaService);
   peluquerias: Peluqueria[] = [];
+
+  //Lista que muestra las peluquerias (resultado del filtro)
+  peluqueriasFiltradas: Peluqueria[] = [];
+
+  //Lo que escribe el usuario en la barra de búsqueda
+  searchTerm: string = '';
 
   constructor() { }
 
@@ -26,9 +33,26 @@ export class HomeComponent implements OnInit {
     this.peluqueriaService.getPeluquerias().subscribe({
       next: (data) => {
         this.peluquerias = data;
+        this.peluqueriasFiltradas = data;
         console.log('Datos cargados:', data);
       },
       error: (err) => console.error('Error al conectar:', err)
     });
+  }
+  //metodo que se ejecuta cada vez que escribes una letra
+
+filtrarResultados() {
+    if (!this.searchTerm) {
+      // Si el buscador está vacío, mostramos todas
+      this.peluqueriasFiltradas = this.peluquerias;
+    } else {
+      // Filtramos: Que el nombre incluya lo que has escrito (ignorando mayúsculas/minúsculas)
+      const termino = this.searchTerm.toLowerCase();
+      
+      this.peluqueriasFiltradas = this.peluquerias.filter(pelu => 
+        pelu.nombre.toLowerCase().includes(termino) || 
+        pelu.direccion.toLowerCase().includes(termino) // (Opcional) También busca por dirección
+      );
+    }
   }
 }
